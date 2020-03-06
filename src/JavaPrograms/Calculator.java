2403b0ee0262b8353abfd3c4af9918f678b39bee
Sriharsha Bikumalla <sriharsha.bikumalla@apdcomms.co.uk>
2020-03-06 15:58:34 +0530

    commit1

---
+++ src/JavaPrograms/Calculator.java
@@ -0,0 +1,26 @@
+package JavaPrograms;
+
+public class Calculator
+{
+	int x=10;
+	int y=20;
+	static int a=15;
+	static int b=25;
+	
+	
+//	public int sum1()
+//	{
+//	return a;
+//		//System.out.println("It performs sum");
+//	}
+	public void sum()
+	{
+		System.out.println("It performs sum");
+	}
+
+	public void sub()
+	{
+		System.out.println("It performs sub");
+	}
+
+}
